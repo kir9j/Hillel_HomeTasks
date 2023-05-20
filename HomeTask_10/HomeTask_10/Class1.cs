cbdecbea8117e04895f60e4a9cb4f831b57112a1
Kyrylo <kyrylolysenko21@gmail.com>
2023-05-20 12:50:41 +0300

    fake update 2

---
+++ HomeTask_10/HomeTask_10/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_10
+{
+    internal class Class1
+    {
+    }
+}
