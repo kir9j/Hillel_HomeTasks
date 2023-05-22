715ce6b659cfe658bfbe570d74d0685ae9ba3ebb
Kyrylo <kyrylolysenko21@gmail.com>
2023-05-22 17:59:40 +0300

    HomeTask_10

---
+++ HomeTask_10/HomeTask_10/GenerateNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_10
+{
+    
+    public class GenerateNumbers
+    {
+        
+        public int doubleCounter;
+        public int singleCounter;
+
+        public void GenerateRandomNumber()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int number = random.Next(1, 101);
+                if (number % 2 == 0)
+                {
+                    doubleCounter++;
+                }
+                else
+                {
+                    singleCounter++;
+                }               
+            }
+        }
+
+        
+        public async Task GenerateRandomAsync()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Run(() => GenerateRandomNumber());
+            }
+            Console.WriteLine($"Кiлькiсть парних чисел: {doubleCounter}\nКiлькiсть непарних чисел: {singleCounter}");
+             
+        }    
+
+    }
+}
--- HomeTask_10/HomeTask_10/Program.cs
+++ HomeTask_10/HomeTask_10/Program.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HomeTask_10
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task Main()
         {
-
-
+            GenerateNumbers generator = new GenerateNumbers();
+            await generator.GenerateRandomAsync();
         }
     }
 }
