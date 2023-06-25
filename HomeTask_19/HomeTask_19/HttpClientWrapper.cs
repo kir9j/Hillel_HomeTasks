e500ef47f449c4764829bfcbeb3224f541cd9c5a
Кирилл Лысенко <kiril228228322@gmail.com>
2023-06-25 14:45:27 +0300

    HomeTask_19

---
+++ HomeTask_19/HomeTask_19/HttpClientWrapper.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net.Http;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+public class HttpClientWrapper
+{
+    public async Task<string> Get(string url)
+    {
+        using (var httpClient = new HttpClient()) 
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Помилка під час виконання запиту {ex.Message}");
+            }
+            finally 
+            { 
+                httpClient.Dispose();
+            }
+            
+            return null;
+        }
+        
+    }
+
+    public async Task<string> Post(string url, string content)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.PostAsync(url, new StringContent(content));
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Помилка під час виконання запиту {ex.Message}");
+            }
+            finally
+            {
+                httpClient.Dispose();
+            }
+
+            return null;
+        }
+    }
+
+    public async Task<string> Put(string url, string content)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.PutAsync(url, new StringContent(content));
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Помилка під час виконання запиту {ex.Message}");
+            }
+            finally
+            {
+                httpClient.Dispose();
+            }
+
+            return null;
+        }
+    }
+
+    public async Task<string> Delete(string url)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.DeleteAsync(url);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Помилка під час виконання запиту {ex.Message}");
+            }
+            finally
+            {
+                httpClient.Dispose();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ HomeTask_19/HomeTask_19/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HomeTask_19;
+
+public class Program 
+{
+    public static async Task Main()
+    {
+        HttpClientWrapper client = new HttpClientWrapper();
+
+        string response = await client.Get("https://api.example.com/data");
+        Console.WriteLine(response);
+
+        string postData = "Some data to be posted";
+        response = await client.Post("https://api.example.com/data", postData);
+        Console.WriteLine(response);
+
+        string putData = "Some data to be put";
+        response = await client.Put("https://api.example.com/data/1", putData);
+        Console.WriteLine(response);
+
+        response = await client.Delete("https://api.example.com/data/1");
+        Console.WriteLine(response);
+    }
+}
