5a3b7425a432261e8b2523053248071f63e76a27
Кирилл Лысенко <kiril228228322@gmail.com>
2023-06-27 14:58:46 +0300

    HomeTask_20

---
+++ HomeTask_20/HomeTask_20/Controllers/UsersController.cs
@@ -0,0 +1,62 @@
+﻿using HomeTask_20;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+public class UsersController : ApiController
+{
+    private List<User> users;
+
+    public UsersController()
+    {
+        users = new List<User>();
+    }
+
+    public IEnumerable<User> Get()
+    {
+        return users;
+    }
+
+    public IHttpActionResult Get(string name)
+    {
+        var user = users.FirstOrDefault(u => u.Name == name);
+        if (user == null)
+            return NotFound();
+
+        return Ok(user);
+    }
+
+    public IHttpActionResult Post(User user)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest("Invalid data.");
+
+        users.Add(user);
+        return Ok();
+    }
+
+    public IHttpActionResult Put(string name, User user)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest("Invalid data.");
+
+        var existingUser = users.FirstOrDefault(u => u.Name == name);
+        if (existingUser == null)
+            return NotFound();
+
+        existingUser.Name = user.Name;
+        existingUser.Age = user.Age;
+
+        return Ok();
+    }
+
+    public IHttpActionResult Delete(string name)
+    {
+        var user = users.FirstOrDefault(u => u.Name == name);
+        if (user == null)
+            return NotFound();
+
+        users.Remove(user);
+        return Ok();
+    }
+}
---
+++ HomeTask_20/HomeTask_20/User.cs
@@ -0,0 +1,8 @@
+﻿namespace HomeTask_20
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
